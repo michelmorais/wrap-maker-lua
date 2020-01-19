1a8cd39af6155d56e6ea67a05b4f6cb49c78041e
michel <michel.braz.morais@gmail.com>
2020-01-19 15:59:45 +0100

    added image

--- test/dummyCalc.cpp
+++ test/dummyCalc.cpp
@@ -1,9 +1,6 @@
 #include "dummyCalc.h"
 
-#include <stdio.h>
-
 int sum(const int v1, const int v2)
 {
-    printf("calc of %d +%d = %d\n",v1,v2,v1 + v2);
     return v1 + v2;
 }
--- test/dummyCalc.h
+++ test/dummyCalc.h
@@ -1,10 +1,6 @@
-#include <stdio.h>
-
 #ifndef DUMMYCALC_HEADER
 #define DUMMYCALC_HEADER
 
-
 int sum(const int v1, const int v2);
-
 
 #endif
